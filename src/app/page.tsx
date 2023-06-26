6cffc19f5a85f9fe4344b42613cda9a5287a4d17
erichen886 <erichen886@gmail.com>
2023-06-26 15:54:38 -0700

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
-  return <main>hi</main>;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      <main className="bg-light w-full min-h-screen">Home</main>
+    </>
+  );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+
+type CustomLinkProps = {
+  href: string;
+  title: string;
+  className?: string;
+};
+
+const CustomLink = ({ href, title, className = "" }: CustomLinkProps) => {
+  const pathName = usePathname();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          pathName === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="ml-4" />
+      </nav>
+      <nav>
+        <Link href="/" target={"_blank"}>
+          LinkedIn
+        </Link>
+        <Link href="/" target={"_blank"}>
+          Github
+        </Link>
+      </nav>
+      {/* TODO: check if this is actually centered */}
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
