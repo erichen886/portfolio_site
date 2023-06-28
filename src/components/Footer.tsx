a531703d2e7f72645cc7973e10812d37a94f7794
erichen886 <erichen886@gmail.com>
2023-06-28 15:02:31 -0700

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,20 @@ const Footer = () => {
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg ">
       <Layout className="py-8 flex items-center justify-between">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-        {/* <Link href="/">Eric Chen</Link> */}
-        <Link href="/">Say Hello</Link>
+        <div className="flex items-center">
+          Build With<span className="text-primary text-2xl px-1">&#9825;</span>
+          by&nbsp;
+          <Link href="/" className="underline underline-offset-2">
+            Eric Chen
+          </Link>
+        </div>
+        <Link
+          href="/"
+          target={"_blank"}
+          className="underline underline-offset-2"
+        >
+          Say Hello
+        </Link>
       </Layout>
     </footer>
   );
